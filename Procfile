210f8fe2ca1bc63bd245b641f8427aa924bab6c3
Rajesh Kumar Mishra <rajeshkumarmishra@Rajeshs-MacBook-Air.local>
2026-01-13 08:11:47 +0530

    Fix: Simplify startup & relax allowed hosts for debugging

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py collectstatic --noinput && python manage.py migrate && gunicorn hewor_project.wsgi --bind 0.0.0.0:$PORT --timeout 120 --access-logfile - --error-logfile - --log-level debug
+web: python manage.py migrate && gunicorn hewor_project.wsgi --bind 0.0.0.0:$PORT --log-file -
--- hewor_project/settings.py
+++ hewor_project/settings.py
@@ -60,8 +60,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-changeme'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-ALLOWED_HOSTS.extend(['hewor.in', 'www.hewor.in', '.railway.app'])
+ALLOWED_HOSTS = ['*'] # Reset to allow all for debugging 502, then restrict later.
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+# ALLOWED_HOSTS.extend(['hewor.in', 'www.hewor.in', '.railway.app', '.up.railway.app'])
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://127.0.0.1,http://localhost').split(',')
 CSRF_TRUSTED_ORIGINS.extend(['https://hewor.in', 'https://www.hewor.in'])
 
