82f2ad1eaa717197f9f10d026468a22ea8a0e9ef
Rajesh Kumar Mishra <rajeshkumarmishra@Rajeshs-MacBook-Air.local>
2026-01-14 10:21:50 +0530

    Switch to CompressedStaticFilesStorage to fix static serving

--- hewor_project/settings.py
+++ hewor_project/settings.py
@@ -196,7 +196,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = "/static/"
 STATIC_ROOT = str(BASE_DIR / "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # ... (omitted)
 
